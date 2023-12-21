b68dbbc57308a1efa40ab4c10614e94f4623d1b0
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-12-21 12:55:29 +0100

    Enabling sign out

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,17 +8,59 @@ import {
     Input,
     Button,
     Avatar,
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
 
 } from '@nextui-org/react';
 
 import {auth} from '@/auth';
+import * as actions from '@/actions';
 
 
 export default async function Header() {
     const session = await auth();
 
+    let authContent: React.ReactNode;
+    if (session?. user ){
+        authContent = <Popover placement="left">
+            <PopoverTrigger>
+            <Avatar src={session.user.image || ''}/>
+            </PopoverTrigger>
+            <PopoverContent>
+                <div className="p-4">
+                    
+                    <form action={actions.signOut}>
+                        <Button type="submit"> SignOut </Button>
+                    </form>
+
+                </div>
+            </PopoverContent>
+        </Popover>
+    } else{
+        authContent = (
+        <>
+        <NavbarItem>
+            <form action={actions.signIn}>
+                <Button type="submit" color="secondary" variant="bordered">
+                    Sign In
+                </Button>
+        </form>
+        </NavbarItem>
+            <form action={actions.signIn}>
+                <Button type="submit" color="primary" variant="flat">
+                    Sign Up
+                </Button>
+            </form>
+        <NavbarItem>
+            
+            </NavbarItem>
+        </>
+        )
+    }
+
     return (
-        <Navbar className="shadow mb-6">
+        <Navbar className="shadow mb-4">
             <NavbarBrand>
                 <Link href="/" className="font-bold">Discussion</Link>
             </NavbarBrand>
@@ -32,14 +74,9 @@ export default async function Header() {
             </NavbarContent >
 
 
-            <NavbarContent justify="end">
-                <NavbarItem>
-                    {
-                    session?. user ?  <div>Signed In</div> : <div>Signed Out</div>
-                    }
-                </NavbarItem>
-                </NavbarContent>
+            <NavbarContent justify="end"> { authContent} </NavbarContent>
+
         </Navbar>
-    )
+    );
     
 }
