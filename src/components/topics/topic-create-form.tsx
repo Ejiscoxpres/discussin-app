4c7dab41c03ddd73328d3aa3b48596fba125a1ff
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-12-22 14:51:03 +0100

    Showing Loading Spinners

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -11,6 +11,7 @@ import{
 } from '@nextui-org/react';
 
 import * as actions from '@/actions';
+import FormButton from '@/components/common/form-button';
 
 export default function TopicCreateForm(){
     const [formState, action] = useFormState(actions.createTopic, {
@@ -43,12 +44,13 @@ export default function TopicCreateForm(){
                         errorMessage = {formState.errors.description?.join(', ')}
                         />
 
-                        {formState.errors._form ?<div className='rounded p-2 bg-red-200 border border-red-400'>
+                        {formState.errors._form ?(<div className='rounded p-2 bg-red-200 border border-red-400'>
                             {formState.errors._form ?.join(', ')}
                         </div> 
-                        : null}
+                        ) : null}
 
-                        <Button type='submit'>Submit</Button>
+                       <FormButton> Save </FormButton>
+                              
                 </div>
                 </form>
                 </PopoverContent>
